852f20b9185b4e6f6d471fd5c373209eeecad9bf
selim akaslan <sakaslan6957@gmail.com>
2021-03-06 23:52:42 +0100

    day01 commit

--- src/day01/C1_Ornek1.java
+++ src/day01/C1_Ornek1.java
@@ -7,7 +7,9 @@ public class C1_Ornek1 {
 		Asagidaki String degiskenini kullanarak  konsolda A L i yazdiriniz. 
 		String  pickName = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		*/ 
-
+		String  name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		System.out.println(name.charAt(0) +" "+ name.charAt(11) +" "+ name.toLowerCase().charAt(8));
 
 	}
 
